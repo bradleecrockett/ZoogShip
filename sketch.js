e6a5d6eb20b90ee80ebc73b27abb5fa9d4544429
Brad C <bcrockett@rjuhsd.us>
2025-01-06 15:54:13 -0800

    created a p5 project

---
+++ sketch.js
@@ -0,0 +1,34 @@
+// ZoogShip
+// Name:
+// Date:
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+//
+
+function setup() {
+  createCanvas(400, 400);
+  frameRate(5);
+}
+
+function draw() {
+  background(220);
+  stroke(0);
+  // Your code here:
+
+  // TODO cockpit
+
+  // TODO UFO body
+
+  // TODO change the fill color to green
+
+  // TODO Alien Head
+
+  // TODO Alien Eyes
+
+  // TODO Alien Mouth
+
+  // TODO UFO landing gear
+
+  // Alien lights (7 lights different colors)
+}
